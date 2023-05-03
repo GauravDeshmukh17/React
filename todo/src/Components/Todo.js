49caa128e1cdca34b143d2d835b8e3440b69d63a
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2023-05-03 21:22:08 +0530

    :whitecheckmark: todo completed

--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
+import './Todo.css'
 
 export default class Todo extends Component {
   constructor(){
+    let d=JSON.parse(localStorage.getItem("data"));
+    // console.log(d[d.length-1].id);
+    // console.log(d);
     super();
+    let uid=(d.length==0) ? 1 : d[d.length-1].id+1
     this.state={
       tasks:[
         {id:1,task:"revise Js"},
         {id:2,task:"revise DSA"}
       ],
-      currTask:""      
+      currTask:"" , 
+      uniqueId:uid 
     }
+    console.log("hello");
+    let b=false;
+    (localStorage.getItem("data")) ? this.state.tasks=JSON.parse(localStorage.getItem("data")) : b=true
+    // console.log(this.state.tasks);
   }
 
   handleChange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     // this.state={
     //   currTask:e.target.value
     // }
@@ -25,13 +35,17 @@ export default class Todo extends Component {
   handleSubmit = () => {
     // console.log("currTAsk "+this.state.currTask);
     // console.log(this.state.tasks);
+    console.log(this.state.uniqueId);
     this.setState({
-      tasks:[...this.state.tasks,{id:this.state.tasks.length+1,task:this.state.currTask}]
+      uniqueId:this.state.uniqueId+1,
+      tasks:[...this.state.tasks,{id:this.state.uniqueId,task:this.state.currTask}]
     },() => {
+      // console.log(this.state.currTask);
       localStorage.setItem("data",JSON.stringify(this.state.tasks));
+      console.log(this.state.uniqueId);
     })
   }
-
+  
   handleDelete = (id) => {
     // console.log(id);
     let filteredArr=this.state.tasks.filter((tasksObj) => {
@@ -40,9 +54,12 @@ export default class Todo extends Component {
     // console.log(filteredArr);
     this.setState({
       tasks:filteredArr
-    })
+    },() => {
+    console.log(filteredArr);
+    localStorage.setItem("data",JSON.stringify(filteredArr));
+  })
   }
-
+  
   render(){
     console.log("render method is called");
     return( // jsx starts
@@ -63,3 +80,6 @@ export default class Todo extends Component {
     )
   }
 }
+
+
+
